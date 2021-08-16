6d8f00f19746a422cf642f0d05d3879e44bc8c8a
SophieWChen <sophiewchen6@gmail.com>
2021-08-16 20:45:58 +0100

    Added testing

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -4,6 +4,10 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get home_home_url
     assert_response :success
+
+    assert_select 'title', 'Pokemon Tracker'
+    asser_select 'h1', 'Pokemon Tracker'
+    assert_select 'p', 'Welcome to the Pokemon Tracker!'
   end
 
 end
