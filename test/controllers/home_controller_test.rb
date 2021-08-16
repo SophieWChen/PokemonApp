f1fc533b6fb70e378fb492e3e272c121848c9c22
SophieWChen <sophiewchen6@gmail.com>
2021-08-16 21:04:38 +0100

    Added more testing

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -6,8 +6,35 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
 
     assert_select 'title', 'Pokemon Tracker'
-    asser_select 'h1', 'Pokemon Tracker'
+    assert_select 'h1', 'Pokemon Tracker'
     assert_select 'p', 'Welcome to the Pokemon Tracker!'
   end
+
+  test "should get contact" do
+    get contact_url
+    assert_response :success
+
+    assert_template layout: 'application'
+
+    assert_select 'title', 'Pokemon Tracker'
+    assert_select 'h1', 'Contact Us'
+    assert_select 'p', 'Complete the following form to get in touch with us.'
+  end
+
+  test "should post request contact but no email" do
+    post request_contact_url
+
+    assert_response :redirect
+    assert_not_empty flash[:alert]
+    assert_nil flash[:notice]
+  end
+
+  test "should post request contact" do
+    post request_contact_url, params: {name:"Jane", email:"janedoe@123.com", telephone: "01234567890", message: "Hello"}
+    assert_response :redirect
+    assert_nil flash[:alert]
+    assert_not_empty flash[:notice]
+  end
+
 
 end
--- test/mailers/contact_mailer_test.rb
+++ test/mailers/contact_mailer_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class ContactMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should return contact email" do
+    mail = COntactMailer.contact_email("janedoe@123.com", "Jane Doe", "01234567890", @message = "Hello")
+    assert_equal ['info@pokemonapp.com'], mail.to
+    assert_equal ['info@pokemonapp.com'], mail.from
+  end
+
 end
